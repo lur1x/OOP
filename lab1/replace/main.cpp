ebe37c5e20e47b0dd64cbb0375ef2866eafb469c
Kirillov <mr.lurix@gmail.com>
2025-03-05 20:05:26 +0300

    lab1\replace: поменял структуру проекта, подправил тесты и новая версия replace

--- lab1/replace/main.cpp
+++ lab1/replace/main.cpp
@@ -1,51 +1,99 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <optional>
+#include <cstring>
+#include <memory>
 
 struct Args {
-    std::string inputFileName;
-    std::string outputFileName;
+    std::unique_ptr<std::istream> input;
+    std::unique_ptr<std::ostream> output;
     std::string searchString;
     std::string replaceString;
 };
 
-std::optional<Args> ParseArgs(int argc, char* argv[])
+void PrintHelp()
 {
-    if (argc != 5)
-    {
-        std::cerr << "Invalid arguments count\n";
-        std::cerr << "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
-        return std::nullopt;
+    auto helpFile = std::ifstream("..utils/help.txt");
+    if (!helpFile.is_open()) {
+        std::cerr << "Error: Failed to open help file" << std::endl;
+        return;
     }
 
-    Args args;
-    args.inputFileName = argv[1];
-    args.outputFileName = argv[2];
-    args.searchString = argv[3];
-    args.replaceString = argv[4];
-
-    return args;
+    std::string line;
+    while (std::getline(helpFile, line))
+    {
+        std::cout << line << std::endl;
+    }
 }
 
-bool initFile(std::ifstream& inputFile, std::ofstream& outputFile,
-              const std::string& inputFileName, const std::string& outputFileName)
+Args GetArgsFromStdin()
 {
-    inputFile.open(inputFileName);
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Failed to open '" << inputFileName << "' for reading\n";
-        return false;
+    std::string searchString;
+    std::string replacementString;
+
+    if (!std::getline(std::cin, searchString)) {
+        throw std::invalid_argument("ERROR");
     }
 
-    outputFile.open(outputFileName);
-    if (!outputFile.is_open())
-    {
-        std::cerr << "Failed to open '" << outputFileName << "' for writing\n";
-        //inputFile.close();
-        return false;
+    if (!std::getline(std::cin, replacementString)) {
+        throw std::invalid_argument("ERROR");
     }
-    return true;
+
+    return {
+            std::make_unique<std::istream>(std::cin.rdbuf()),
+            std::make_unique<std::ostream>(std::cout.rdbuf()),
+            searchString,
+            replacementString
+    };
+}
+
+Args GetArgsFromArgv(char* argv[])
+{
+
+    auto inputFile = std::make_unique<std::ifstream>(argv[1]);
+    if (!inputFile->is_open()) {
+        throw std::ios_base::failure("ERROR");
+    }
+
+    auto outputFile = std::make_unique<std::ofstream>(argv[2]);
+    if (!outputFile->is_open()) {
+        throw std::ios_base::failure("ERROR" );
+    }
+
+    std::string searchString = argv[3];
+    std::string replacementString = argv[4];
+
+    return {
+            std::move(inputFile),
+            std::move(outputFile),
+            searchString,
+            replacementString
+    };
+}
+
+Args ParseArgs(int argc, char* argv[])
+{
+    const int STDIN_ARGS_COUNT = 1;
+    const int HELP_ARGS_COUNT = 2;
+    const int CLI_ARGS_COUNT = 5;
+
+    if (argc == STDIN_ARGS_COUNT)
+    {
+        return GetArgsFromStdin();
+    }
+    else if ((argc == HELP_ARGS_COUNT) && (std::strcmp(argv[1], "-h") == 0))
+    {
+        PrintHelp();
+        std::exit(0);
+    }
+    else if (argc == CLI_ARGS_COUNT)
+    {
+        return GetArgsFromArgv(argv);
+    }
+    else
+    {
+        throw std::invalid_argument("ERROR");
+    }
 }
 
 std::string ReplaceString(const std::string& subject,
@@ -53,17 +101,15 @@ std::string ReplaceString(const std::string& subject,
 {
     if (searchString.empty())
     {
-         return subject;
+        return subject;
     }
 
     size_t pos = 0;
     std::string result;
-    //result.reserve(subject.length());
 
     while (pos < subject.length())
     {
         size_t foundPos = subject.find(searchString, pos);
-        //result.append(subject, pos, foundPos - pos);
         if (foundPos == std::string::npos)
         {
             result.append(subject, pos);
@@ -78,55 +124,31 @@ std::string ReplaceString(const std::string& subject,
     return result;
 }
 
-bool CopyStreamWithReplacement(std::istream& input, std::ostream& output,
+void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
                                const std::string& searchString, const std::string& replacementString)
 {
     std::string line;
-
     while (std::getline(input, line))
     {
-        output << ReplaceString(line, searchString, replacementString) << "\n";
-        if (!output)
-        {
-            std::cerr << "Failed to write data to output file\n";
-            return false;
-        }
+        output << ReplaceString(line, searchString, replacementString) << std::endl;
     }
 
-    if (input.bad())
-    {
-        std::cerr << "Failed to read data from input file\n";
-        return false;
+    if (input.bad()) {
+        throw std::runtime_error("ERROR");
     }
-    return true;
 }
 
 int main(int argc, char* argv[])
 {
-    auto args = ParseArgs(argc, argv);
-
-    if (!args)
+    try {
+        auto args = ParseArgs(argc, argv);
+        CopyStreamWithReplacement(*args.input, *args.output,
+                                  args.searchString, args.replaceString);
+        return 0;
+    }
+    catch (const std::exception& exception)
     {
+        std::cerr  << exception.what() << std::endl;
         return 1;
     }
-
-    std::ifstream inputFile;
-    std::ofstream outputFile;
-
-    if (!initFile(inputFile, outputFile, args->inputFileName, args->outputFileName))
-    {
-        return 1;
-    }
-
-    std::string search = argv[3];
-    std::string replace = argv[4];
-
-    if (!CopyStreamWithReplacement(inputFile, outputFile, search, replace))
-    {
-        //inputFile.close();
-        //outputFile.close();
-        return 1;
-    }
-
-    return 0;
 }
--- lab1/replace/utils/test/test.bat
+++ lab1/replace/utils/test/test.bat
@@ -1,86 +1,86 @@
-set PROGRAM="..\cmake-build-debug\replace.exe"
+set PROGRAM="..\..\cmake-build-debug\replace.exe"
 
-%PROGRAM% > nul
-if NOT ERRORLEVEL 1 goto err
+if %PROGRAM%=="" (
+	exit /B 1
+)
 
 rem Not enough arguments in command line
-%PROGRAM% test1\input.txt "%TEMP%\output.txt" 123
+%PROGRAM% test-data\test1\input.txt "%TEMP%\output.txt" 123
 echo run first test
 if NOT ERRORLEVEL goto err
 echo first test ok
 
 rem Too many arguments on the command line
-%PROGRAM% test2\input.txt "%TEMP%\output.txt" "%TEMP\output.txt%" 12 123
+%PROGRAM% test-data\test2\input.txt "%TEMP%\output.txt" "%TEMP\output.txt%" 12 123
 echo run second test
 if NOT ERRORLEVEL goto err
 echo second test ok
 
 rem Non-existent input file
-%PROGRAM% test3\input1.txt "%TEMP%\output.txt" 12 123
+%PROGRAM% test-data\test3\input1.txt "%TEMP%\output.txt" 12 123
 echo run third test
 if NOT ERRORLEVEL goto err
 echo third test ok
 
 rem Incorrect output file
-%PROGRAM% test4\input.txt replace.exe 12 123
+%PROGRAM% test-data\test4\input.txt replace.exe 12 123
 echo run fourth test
 if NOT ERRORLEVEL goto err
 echo fourth test ok
 
 rem Empty input file
-%PROGRAM% test5\empty.txt "%TEMP%\output.txt" 12 123
+%PROGRAM% test-data\test5\empty.txt "%TEMP%\output.txt" 12 123
 echo run fifth test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test5\empty_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test5\empty_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo fifth test ok
 
 rem No replacement
-%PROGRAM% test6\no-replacement.txt "%TEMP%\output.txt" 12 123
+%PROGRAM% test-data\test6\no-replacement.txt "%TEMP%\output.txt" 12 123
 echo run sixth test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test6\no-replacement_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test6\no-replacement_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo sixth test ok
 
 rem Empty search string
-%PROGRAM% test7\empty-search-string.txt "%TEMP%\output.txt" "" 123
+%PROGRAM% test-data\test7\empty-search-string.txt "%TEMP%\output.txt" "" 123
 echo run seventh test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test7\empty-search-string_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test7\empty-search-string_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo seventh test ok
 
 rem Replacement for one line
-%PROGRAM% test8\one-line.txt "%TEMP%\output.txt" cat dog
+%PROGRAM% test-data\test8\one-line.txt "%TEMP%\output.txt" cat dog
 echo run eighth test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test8\one-line_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test8\one-line_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo eighth test ok
 
 rem Replacement for multline lines
-%PROGRAM% test9\multline.txt "%TEMP%\output.txt" cat dog
+%PROGRAM% test-data\test9\multline.txt "%TEMP%\output.txt" cat dog
 echo run ninth test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test9\multline_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test9\multline_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo ninth test ok
 
-
 rem Сyclicity check
-%PROGRAM% test10\cyclicity.txt "%TEMP%\output.txt" ma mama
+%PROGRAM% test-data\test10\cyclicity.txt "%TEMP%\output.txt" ma mama
 echo run tenth test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test10\cyclicity_out.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test10\cyclicity_out.txt >nul
 if ERRORLEVEL 1 goto err
 echo tenth test ok
 
 rem Сheck line "12312312345"
-%PROGRAM% test11\input.txt "%TEMP%\output.txt" 1231234 XYZ
+%PROGRAM% test-data\test11\input.txt "%TEMP%\output.txt" 1231234 XYZ
 echo run eleventh test
 if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\output.txt" test11\output.txt >nul
+fc.exe "%TEMP%\output.txt" test-data\test11\output.txt >nul
 if ERRORLEVEL 1 goto err
 echo eleventh test ok
 
