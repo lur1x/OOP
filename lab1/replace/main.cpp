077e41955553607291ef9f2f2715b63d367ce7c9
Kirillov <mr.lurix@gmail.com>
2025-03-12 14:46:57 +0300

    новая версия программы: поменял структуры  и переписал часть кода с выбором режима

--- lab1/replace/main.cpp
+++ lab1/replace/main.cpp
@@ -2,104 +2,24 @@
 #include <fstream>
 #include <string>
 #include <cstring>
-#include <memory>
-
-struct Args {
-    std::unique_ptr<std::istream> input;
-    std::unique_ptr<std::ostream> output;
-    std::string searchString;
-    std::string replaceString;
-};
 
 void PrintHelp()
 {
-    auto helpFile = std::ifstream("..utils/help.txt");
+    auto helpFile = std::ifstream("../utils/help.txt");
     if (!helpFile.is_open()) {
-        std::cerr << "Error: Failed to open help file" << std::endl;
-        return;
+        throw std::runtime_error("ERROR\n");
     }
-
+    
     std::string line;
-    while (std::getline(helpFile, line))
-    {
+    while (std::getline(helpFile, line)) {
         std::cout << line << std::endl;
-    }
-}
-
-Args GetArgsFromStdin()
-{
-    std::string searchString;
-    std::string replacementString;
-
-    if (!std::getline(std::cin, searchString)) {
-        throw std::invalid_argument("ERROR");
-    }
-
-    if (!std::getline(std::cin, replacementString)) {
-        throw std::invalid_argument("ERROR");
-    }
-
-    return {
-            std::make_unique<std::istream>(std::cin.rdbuf()),
-            std::make_unique<std::ostream>(std::cout.rdbuf()),
-            searchString,
-            replacementString
-    };
-}
-
-Args GetArgsFromArgv(char* argv[])
-{
-
-    auto inputFile = std::make_unique<std::ifstream>(argv[1]);
-    if (!inputFile->is_open()) {
-        throw std::ios_base::failure("ERROR");
-    }
-
-    auto outputFile = std::make_unique<std::ofstream>(argv[2]);
-    if (!outputFile->is_open()) {
-        throw std::ios_base::failure("ERROR" );
-    }
-
-    std::string searchString = argv[3];
-    std::string replacementString = argv[4];
-
-    return {
-            std::move(inputFile),
-            std::move(outputFile),
-            searchString,
-            replacementString
-    };
-}
-
-Args ParseArgs(int argc, char* argv[])
-{
-    const int STDIN_ARGS_COUNT = 1;
-    const int HELP_ARGS_COUNT = 2;
-    const int CLI_ARGS_COUNT = 5;
-
-    if (argc == STDIN_ARGS_COUNT)
-    {
-        return GetArgsFromStdin();
-    }
-    else if ((argc == HELP_ARGS_COUNT) && (std::strcmp(argv[1], "-h") == 0))
-    {
-        PrintHelp();
-        std::exit(0);
-    }
-    else if (argc == CLI_ARGS_COUNT)
-    {
-        return GetArgsFromArgv(argv);
-    }
-    else
-    {
-        throw std::invalid_argument("ERROR");
     }
 }
 
 std::string ReplaceString(const std::string& subject,
                           const std::string& searchString, const std::string& replacementString)
 {
-    if (searchString.empty())
+    if (searchString.empty() || searchString == replacementString)
     {
         return subject;
     }
@@ -107,11 +27,9 @@ std::string ReplaceString(const std::string& subject,
     size_t pos = 0;
     std::string result;
 
-    while (pos < subject.length())
-    {
+    while (pos < subject.length()) {
         size_t foundPos = subject.find(searchString, pos);
-        if (foundPos == std::string::npos)
-        {
+        if (foundPos == std::string::npos) {
             result.append(subject, pos);
             break;
         }
@@ -128,9 +46,12 @@ void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
                                const std::string& searchString, const std::string& replacementString)
 {
     std::string line;
-    while (std::getline(input, line))
-    {
+    while (std::getline(input, line)) {
         output << ReplaceString(line, searchString, replacementString) << std::endl;
+        if (!output)
+        {
+            throw std::runtime_error("ERROR\n");
+        }
     }
 
     if (input.bad()) {
@@ -138,17 +59,85 @@ void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
     }
 }
 
+void FileMode(const std::string& inputPath, const std::string& outputPath,
+                    const std::string& searchString, const std::string& replaceString)
+{
+    auto inputFile = std::ifstream(inputPath);
+    if (!inputFile.is_open())
+    {
+        throw std::ios_base::failure("ERROR");
+    }
+
+    auto outputFile = std::ofstream(outputPath);
+    if (!outputFile.is_open())
+    {
+        throw std::ios_base::failure("ERROR");
+    }
+
+    CopyStreamWithReplacement(inputFile, outputFile, searchString, replaceString);
+}
+
+void PrintFile(std::ifstream& output)
+{
+    std::string line;
+    while (std::getline(output, line))
+    {
+        std::cout << line << std::endl;
+    }
+}
+
+void StdinMode()
+{
+    std::string searchString;
+    std::string replaceString;
+
+    if (!std::getline(std::cin, searchString))
+    {
+        throw std::invalid_argument("ERROR");
+    }
+    if (!std::getline(std::cin, replaceString))
+    {
+        throw std::invalid_argument("ERROR");
+    }
+
+    std::ofstream temFile("../output.txt");
+    CopyStreamWithReplacement(std::cin, temFile, searchString, replaceString);
+    temFile.close();
+
+    std::ifstream output("../output.txt");
+    PrintFile(output);
+    output.close();
+}
+
 int main(int argc, char* argv[])
 {
-    try {
-        auto args = ParseArgs(argc, argv);
-        CopyStreamWithReplacement(*args.input, *args.output,
-                                  args.searchString, args.replaceString);
-        return 0;
+    try
+    {
+        const int STDIN_ARGS_COUNT = 1;
+        const int HELP_ARGS_COUNT = 2;
+        const int CLI_ARGS_COUNT = 5;
+
+        if (argc == HELP_ARGS_COUNT && std::strcmp(argv[1], "-h") == 0)
+        {
+            PrintHelp();
+        }
+        else if (argc == CLI_ARGS_COUNT)
+        {
+            FileMode(argv[1], argv[2], argv[3], argv[4]);
+        }
+        else if (argc == STDIN_ARGS_COUNT)
+        {
+            StdinMode();
+        }
+        else
+        {
+            throw std::invalid_argument("ERROR");
+        }
     }
     catch (const std::exception& exception)
     {
-        std::cerr  << exception.what() << std::endl;
+        std::cerr << exception.what() << std::endl;
         return 1;
     }
+    return 0;
 }
